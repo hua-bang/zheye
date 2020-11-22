3fb9eb6655ee140a79595e31609335b6e1e10e5b
hua-bang <qq153153>
2020-11-22 11:20:35 +0800

    完成区域外点击隐藏

--- zheye/src/components/Dropdown.vue
+++ zheye/src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <!-- click.prevent是为了防止默认事件 -->
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{title}}</a>
     <ul class="dropdown-menu" :style="{display:'block'}" v-if="isOpen">
@@ -8,7 +8,7 @@
   </div>
 </template>
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, onMounted, onUnmounted } from 'vue'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -18,13 +18,31 @@ export default defineComponent({
     }
   },
   setup () {
+    // 这里是为了后面的ref能是HTMLElement类型
+    const dropdownRef = ref<null | HTMLElement>(null)
     const isOpen = ref(false)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const handler = (e: MouseEvent) => {
+      // 1.检查有没有dropdown这个元素
+      if (dropdownRef.value) {
+        // 2.看看点击的属性是否属于这个dropdown,不属于且isOpen值为true,则改为false
+        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+          isOpen.value = false
+        }
+      }
+    }
+    onMounted(() => {
+      document.addEventListener('click', handler)
+    })
+    onUnmounted(() => {
+      document.removeEventListener('click', handler)
+    })
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      dropdownRef
     }
   }
 })
